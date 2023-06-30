96b7f031227354a9782de1908a733aeb19b9ee3a
maksimov-m <maks6863@gmail.com>
2023-06-30 19:40:23 +0500

    доработка сайта

--- sale_product_site/product_list/templatetags/product_list_tags.py
+++ sale_product_site/product_list/templatetags/product_list_tags.py
@@ -8,4 +8,5 @@ register = template.Library()
 def get_categories():
     r = requests.get(f'http://45.143.94.139/Discount/Products/Types')
     categories = r.json()
+    mass = []
     return categories
